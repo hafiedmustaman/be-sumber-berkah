6d1fdb3dc977c5984f51362de20f0499f8e8ab21
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 05:45:01 +0700

    menambahkan 8 field untuk table products

--- database/migrations/2023_10_21_220804_create_products_table.php
+++ database/migrations/2023_10_21_220804_create_products_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('image'); // field ini menggunakan type data string dan digunakan untuk menyimpan gambar dari produk.
+            $table->string('title'); // field ini menggunakan type data string dan digunakan untuk menyimpan judul dari produk.
+            $table->string('slug'); // field ini menggunakan type data string dan digunakan untuk membuat URL produk agar lebih SEO friendly.
+            $table->unsignedBigInteger('category_id'); // field ini menggunakan type data unsignedBigInteger dan ini akan digunakan untuk melakukan relasi dengan table categories.
+            $table->text('content'); // field ini menggunakan type data text dan digunakan untuk menyimpan deskripsi dari produk.
+            $table->bigInteger('weight'); // field ini menggunakan type data biginteger dan digunakan untuk menyimpan berat dari produk.
+            $table->bigInteger('price'); // field ini menggunakan type data bigInteger dan digunakan untuk menyimpan harga dari produk.
+            $table->integer('discount')->nullable(); // field ini menggunakan type data integer dan di set menjadi nullable, yang artinya boleh di isi dan juga boleh di kosongi atau null, field ini digunakan untuk menentukan berapa % discount produk yang akan dijual.
             $table->timestamps();
         });
     }
