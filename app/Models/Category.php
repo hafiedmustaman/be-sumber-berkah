2c9538d3deaac14a97bf7881b10780d0d3fad538
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 04:45:56 +0700

    menambahkan properti  dan function image()

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    // $fillable untuk memberikan izin agar field-field dapat melakukan manipulasi data, seperti menambah data, edit, update dan juga hapus.
+    protected $fillable = [
+        'name', 'slug', 'image'
+    ];
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    // function image() untuk mendapatkan full-path / URL dari image yang ada di dalam table categories. Method tersebut bernama Accessor.
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/categories/' . $value),
+        );
+    }
 }
