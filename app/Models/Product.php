0f927398fbd681a185682cf49eb340b5ce55a9a2
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 05:39:35 +0700

    menambahkan properti  dan function image() pada Model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    // $fillable untuk memberikan izin agar field-field dapat melakukan manipulasi data, seperti menambah data, edit, update dan juga hapus.
+    protected $fillable = [
+        'image', 'title', 'slug', 'category_id', 'content', 'weight', 'price', 'discount'
+    ];
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    // function image() untuk mendapatkan full-path / URL dari image yang ada di dalam table products. Method tersebut bernama Accessor.
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/products/' . $value),
+        );
+    }
 }
