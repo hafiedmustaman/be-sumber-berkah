2a9be626b8bf6d923b457a5226bc01617c6543ea
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 21:25:00 +0700

    menambahkan 5 field untuk table carts

--- database/migrations/2023_10_22_141319_create_carts_table.php
+++ database/migrations/2023_10_22_141319_create_carts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('product_id'); // field ini menggunakan type data unsignedInteger dan digunakan untuk menyimpan product_id dan melakukan relasi dengan table products.
+            $table->unsignedInteger('customer_id'); // field ini menggunakan type data unsignedInteger dan digunakan untuk menyimpan customer_id dan melakukan relasi dengan table customers.
+            $table->integer('quantity'); // field ini menggunakan type data integer dan digunakan untuk menyimpan jumlah dari pesanan yang dimasukkan ke cart.
+            $table->bigInteger('price'); // field ini menggunakan type data bigInteger dan digunakan untuk menyimpan harga dari product yang dipesan.
+            $table->bigInteger('weight'); // field ini menggunakan type data bigInteger dan digunakan untuk menyimpan berat dari product yang dipesan.
             $table->timestamps();
         });
     }
