4c586ae8fbdf8754ff61d102a87eff65c6bcd2f1
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 14:35:05 +0700

    menambahkan 5 field untuk table customers

--- database/migrations/2023_10_22_070517_create_customers_table.php
+++ database/migrations/2023_10_22_070517_create_customers_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('name'); // field ini menggunakan type data string dan digunakan untuk menyimpan nama customer.
+            $table->string('email')->unique(); // field ini menggunakan type data string dan menggunakan unique yang artinya isi dari field ini tidak boleh ada yang sama.
+            $table->timestamp('email_verified_at')->nullable(); // field ini menggunakan type data timestamp dan digunakan untuk mendapatkan tanggal saat email berhasil di verifikasi.
+            $table->string('password'); // field ini menggunakan type data string dan digunakan untuk menyimpan data password.
+            $table->rememberToken(); // field ini akan digunakan untuk menyimpan token saat melakukan reset password.
             $table->timestamps();
         });
     }
