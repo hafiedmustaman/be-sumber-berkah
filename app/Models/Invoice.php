21d9f6707b63ea6315bbc9a2107e94a70002055d
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 08:18:10 +0700

    menambahkan properti  pada Model Invoice

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    // $fillable difungsikan agar field-field tersebut dapat memanipulasi data ke dalam table database.
+    protected $fillable = [
+        'invoice', 'customer_id', 'courier', 'service', 'cost_courier', 'weight', 'name', 'phone', 'province', 'city', 'address', 'status', 'snap_token', 'grand_total'
+    ];
 }
