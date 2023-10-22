848ba1c0858c6cd425f262cf145893c143f26c7f
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 08:21:35 +0700

    menambahkan 14 field untuk table invoices

--- database/migrations/2023_10_22_005618_create_invoices_table.php
+++ database/migrations/2023_10_22_005618_create_invoices_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->text('invoice'); // field ini menggunakan type data string, dan ini akan digunakan untuk menyimpan no. invoice dari pemesanan.
+            $table->unsignedBigInteger('customer_id'); // field ini menggunakan type data unsignedBigInteger dan ini akan digunakan untuk relasi dengan table customers.
+            $table->string('courier'); // field ini menggunakan type data string dan ini akan digunakan untuk menyimpan jenis kurir yang digunakan.
+            $table->string('service'); // field ini menggunakan type data strring dan ini akan digunakan untuk menyimpan data layanan kurir, contohnya misalkan JNE, ada layanan REG, OKE dan lain-lain.
+            $table->bigInteger('cost_courier'); // field ini menggunakan type data bigInteger dan digunakan untuk menyimpan biaya dari ongkos kirim.
+            $table->integer('weight'); // field ini menggunakan type data integer dan digunakan untuk menyimpan berat dar product yang di pesan, karena ini akan dijadikan acuan untuk menghitung biaya ongkos kirim.
+            $table->string('name'); // field ini menggunakan type data string dan digunakan untuk menyimpan nama dari pemesan.
+            $table->bigInteger('phone'); // field ini menggunakan type data bigInteger dan digunakan untuk menyimpan no. telepon dari pemesan.
+            $table->integer('province'); //  field ini menggunakan type data integer dan digunakan untuk menyimpan ID provinsi dari pemesan.
+            $table->integer('city'); //  field ini menggunakan type data integer dan digunakan untuk menyimpan ID kota dari pemesan.
+            $table->text('address'); // field ini menggunakan type data text dan digunakan untuk menyimpan alamat lengkap dari pemesan.
+            $table->enum('status', array('pending', 'success', 'failed', 'expired')); // field ini menggunakan type data enum dan untuk isinya adalah, pending, success, expired dan failed. Field ini akan otomatis update dengan response yang di dapatkan dari payment gateway (Midtrans).
+            $table->string('snap_token')->nullable(); //  field ini menggunakan type data string dan ini akan digunakan untuk menyimpan snap token yang di generate dari payment gateway dan ini kita jadikan nullable yang artinya tidak wajib diisi.
+            $table->bigInteger('grand_total'); // field ini menggunakan type data bigInteger dan digunakan untuk menyimpan total dari pemesan.
             $table->timestamps();
         });
     }
