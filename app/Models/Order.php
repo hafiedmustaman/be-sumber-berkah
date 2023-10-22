785876f32d441b4367c1fdbba196bdba297c444b
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 09:05:07 +0700

    menambahkan properti  pada Model Order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    // $fillable difungsikan untuk mengizinkan field-field agar bisa memanipulasi data ke dalam database.
+    protected $fillable = [
+        'invoice_id', 'invoice', 'product_id', 'product_name', 'image', 'qty', 'price'
+    ];
 }
