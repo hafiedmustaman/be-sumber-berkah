b0548d7c476c5453ec44df76b3ddad51cda6a5d4
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 09:07:46 +0700

    menambahkan 7 field untuk table orders

--- database/migrations/2023_10_22_012540_create_orders_table.php
+++ database/migrations/2023_10_22_012540_create_orders_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('invoice_id'); // field ini menggunakan type data unsignedBigInteger dan ini akan digunakan untuk melakukan relasi dengan table invoices.
+            $table->string('invoice'); // field ini menggunakan type data string dan digunakan untuk menyimpan no. invoice.
+            $table->unsignedBigInteger('product_id'); // field ini menggunakan type data unsignedBigInteger dan digunakan untuk relasi dengan table products.
+            $table->string('product_name'); // field ini menggunakan type data string dan digunakan untuk menyimpan nama product.
+            $table->string('image'); // field ini menggunakan type data string dan digunakan untuk menyimpan nama image dari product.
+            $table->integer('qty'); // field ini menggunakan type data integer dan digunakan untuk menyimpan jumlah quantity dari product yang dipesan.
+            $table->integer('price'); // field ini menggunakan type data integer dan digunakan untuk menyimpan harga dari product yang dipesan.
             $table->timestamps();
         });
     }
