be950d7924c03c0e02dd3f5c1b96c43a1e59478e
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 14:31:03 +0700

    menambahkan 2 properti pada Model Customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable; // import Foundation Auth Laravel
 
-class Customer extends Model
+class Customer extends Authenticatable // Authenticatable digunakan untuk melakukan Authentication.
 {
     use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    // $fillable digunakan untuk mengizinkan field-field di dalam table dapat memanipulasi data, seperti insert, read, edit, update dan delete.
+    protected $fillable = [
+        'name', 'email', 'password'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    // $hidden digunakan untuk menyembunyikan field-field saat ada request ke dalam table customers
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
