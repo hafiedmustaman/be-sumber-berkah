52dd6b59c34515cf5b0db7b042dfe9499ce01d94
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 21:55:52 +0700

    menambahkan properti pada Model City

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    // $fillable digunakan untuk mengizinkan field-field yang ada di dalamnya agar bisa melakukan manipulasi data ke dalam table, seperti insert, edit, update dan delete.
+    protected $fillable = [
+        'province_id', 'city_id', 'name'
+    ];
 }
