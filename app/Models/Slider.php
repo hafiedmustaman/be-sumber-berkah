3ff41478c9ed03a9fdf54fe0ccb17cda4b7a89fd
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 21:06:05 +0700

    menambahkan properti dan function pada Model Slider

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
 {
     use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    // $fillable digunakan agar field-field yang di tentukan dapat memanipulasi data ke dalam database, seperti insert, edit, update dan delete data.
+    protected $fillable = [
+        'image', 'link'
+    ];
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    // function image() digunakan untuk mengambil full PATH / URL image untuk data slider.
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/sliders/' . $value),
+        );
+    }
 }
