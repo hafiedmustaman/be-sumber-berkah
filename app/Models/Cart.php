4572f1a24c7e2b667363143cb9634522666016d4
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-10-22 21:22:44 +0700

    menambahkan properti pada Model Cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    // $fillable difungsikan agar field-field yang dimasukkan di dalam array tersebut dalam melakukan manipulasi data ke dalam table database.
+    protected $fillable = [
+        'product_id', 'customer_id', 'price', 'quantity', 'weight'
+    ];
 }
