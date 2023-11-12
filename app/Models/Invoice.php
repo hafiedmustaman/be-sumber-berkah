9f21b7861ebe8cf0b539d6577bda9837ed645060
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-11-12 17:29:23 +0700

    One To Many (Inverse) Table Invoices ke Table Orders

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -18,4 +18,15 @@ class Invoice extends Model
     protected $fillable = [
         'invoice', 'customer_id', 'courier', 'service', 'cost_courier', 'weight', 'name', 'phone', 'province', 'city', 'address', 'status', 'snap_token', 'grand_total'
     ];
+
+    /**
+     * order
+     *
+     * @return void
+     */
+    // digunakan dan difungsikan untuk memberitahukan ke sistem, bahwa kita akan membuat relasi Many ke model Order atau table orders.
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -18,4 +18,15 @@ class Order extends Model
     protected $fillable = [
         'invoice_id', 'invoice', 'product_id', 'product_name', 'image', 'qty', 'price'
     ];
+
+    /**
+     * invoice
+     *
+     * @return void
+     */
+    // difungsikan untuk memberitahukan sistem, bahwa model Order atau table orders ini dimiliki dan terhubung dengan model Invoice atau tables invoices.
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
 }
