b6f860930e1fb5e96b59da2ad7fa96c152a1de3f
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-11-25 11:30:54 +0700

    membuat Route Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
+namespace App\Http\Controllers\Admin;
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+/**
+ * route for admin
+ */
+
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () { // <-- Prefix "admin" digunakan untuk menambahkan kata admin di awalan URL.
+
+    //group route with middleware "auth"
+    Route::group(['middleware' => 'auth'], function() { // <-- route tersebut hanya bisa di akses jika sudah melakukan authentication atau login.
+        
+        //route dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+    });
 });
