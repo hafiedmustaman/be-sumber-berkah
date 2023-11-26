8b3b9ade588f81493df854009887d59540b69d75
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-11-26 11:37:57 +0700

    membuat Route API Authentication

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+namespace App\Http\Controllers\Api;
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ * Route API Auth
+ */
+Route::post('/login', [AuthController::class, 'login'])->name('api.customer.login');
+Route::post('/register', [AuthController::class, 'register'])->name('api.customer.register');
+Route::get('/user', [AuthController::class, 'getUser'])->name('api.customer.user');
