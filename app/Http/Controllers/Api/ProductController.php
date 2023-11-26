d615e8b3fb85c05e61889c99089c3941f34690c0
Hafied Mustaman <hafiedmustaman@gmail.com>
2023-11-26 15:34:31 +0700

    membuat Controller API Product

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index() // untuk menampilkan semua data product.
+    {
+        $products = Product::latest()->get();
+        return response()->json([
+            'success'   => true,
+            'message'   => 'List Data Products',
+            'products'  => $products
+        ], 200);
+    }
+    
+    /**
+     * show
+     *
+     * @param  mixed $slug
+     * @return void
+     */
+    public function show($slug) // untuk menampilkan detail dari data product.
+    {
+        $product = Product::where('slug', $slug)->first();
+
+        if($product) {
+
+            return response()->json([
+                'success' => true,
+                'message'   => 'Detail Data Product',
+                'product' => $product
+            ], 200);
+
+        } else {
+
+            return response()->json([
+                'success' => false,
+                'message'   => 'Data Product Tidak Ditemukan',
+            ], 404);
+
+        }
+    }
+}
